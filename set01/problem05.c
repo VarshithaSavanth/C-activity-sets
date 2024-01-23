22069c2c41aa8b2b37c1e72885d8287ceb175cd4
Varshitha Savanth <varshithas.bsc23@rvu.edu.in>
2024-01-23 22:22:58 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -48,5 +48,5 @@ int compare(int a, int b, int c)
 
 void output(int a, int b, int c, int largest)
 {
-  printf("The largest among %d, %d, and %d is %d", a, b, c, largest);
+  printf("The largest among %d, %d, and %d is : %d\n", a, b, c, largest);
 }
