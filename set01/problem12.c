750e8cce7b16ecb74e091acbf8caae942ad75328
Varshitha Savanth <varshithas.bsc23@rvu.edu.in>
2024-01-24 23:21:17 +0530

    test

--- set01/problem12.c
+++ set01/problem12.c
@@ -14,3 +14,64 @@ void input_n_complex(int n, Complex c[n]);
 Complex add(Complex a, Complex b);
 Complex add_n_complex(int n, Complex c[n]);
 void output(int n, Complex c[n], Complex result);
+
+int main() 
+{
+    int n = get_n();
+    Complex c[n];
+    input_n_complex(n, c);
+    Complex sum = add_n_complex(n, c);
+    output(n, c, sum);
+    return 0;
+}
+int get_n()
+{
+    int n;
+    printf("Enter the number of complex numbers : ");
+    scanf("%d", &n);
+    return n;
+}
+Complex input_complex()
+{
+    Complex c;
+    printf("Enter the real part : ");
+    scanf("%f", &c.real);
+    printf("Enter the imaginary part : ");
+    scanf("%f", &c.imaginary);
+    return c;
+}
+
+void input_n_complex(int n, Complex c[n]) 
+{
+    for (int i = 0; i < n; i++) 
+    {
+        printf("Enter Complex Number %d:\n", i + 1);
+        c[i] = input_complex();
+    }
+}
+
+Complex add(Complex a, Complex b) 
+{
+    Complex result;
+    result.real = a.real + b.real;
+    result.imaginary = a.imaginary + b.imaginary;
+    return result;
+}
+
+Complex add_n_complex(int n, Complex c[n]) 
+{
+    Complex sum;
+    for (int i = 0; i < n; i++) 
+    {
+        sum = add(sum, c[i]);
+    }
+    return sum;
+}
+
+void output(int n, Complex c[n], Complex result) 
+{
+    for (int i = 0; i < n; i++) {
+        printf("%.0f+%.0fi + ", c[i].real, c[i].imaginary);
+    }
+		printf(" is %.0f+%.0fi", result.real, result.imaginary);
+}
