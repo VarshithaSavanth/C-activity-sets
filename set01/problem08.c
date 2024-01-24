73557cf5c7355ba40eb944f36219c00607047335
Varshitha Savanth <varshithas.bsc23@rvu.edu.in>
2024-01-24 22:45:11 +0530

    test

--- set01/problem08.c
+++ set01/problem08.c
@@ -1,52 +1,50 @@
 // 8] Write a C program to find sum of _n_ different numbers entered by the user.
 
+
 #include<stdio.h>
 
 int input_array_size();
 void input_array(int n, int a[n]);
 int sum_n_array(int n, int a[n]);
-void output(int n, int a[n],int sum);
+void output(int n, int a[n], int sum);
 
 int main()
 {
-  int n = input_array_size();
-  int a[n];
-  input_array(n, a);
-  int sum = sum_n_array(n, a);
-  output(n, a, sum);
-  return 0;
+    int n = input_array_size();
+    int a[n];
+    input_array(n,a);
+    int sum = sum_n_array(n,a);
+    output(n,a,sum);
+    return 0;
 }
-
 int input_array_size()
 {
-  int n;
-  printf("Enter the size of array : ");
-  scanf("%d", &n);
-  return 0;
+    int n;
+    printf("Enter the size of array : ");
+    scanf("%d", &n);
+    return n;
 }
-
 void input_array(int n, int a[n])
 {
-  for (int i=0 ; i<n ; i++)
-  {
-    printf("Enter the values of array : ");
-    scanf("%d", &a[i]);
-  }
+    int i=0;
+    for (i=0 ; i<n ; i++)
+    {
+        printf("Enter the values of array : \n");
+        scanf("%d", &a[i]);
+    }
 }
-
 int sum_n_array(int n, int a[n])
 {
-  int sum=0;
-  for (int i=0 ; i<n ; i++)
-  {
-    sum += a[i];
-  }
-  return sum;
+    int sum = 0;
+    for (int i=0 ; i<n ; i++)
+    {
+          sum += a[i];
+    } 
+    return sum;
 }
-
-void output(int n, int a[n],int sum)
+void output(int n, int a[n], int sum)
 {
-    printf("Sum of numbers: ");
+      printf("Sum of numbers: ");
     for (int i = 0; i < n; i++) 
     {
         printf("%d", a[i]);
@@ -57,4 +55,3 @@ void output(int n, int a[n],int sum)
     }
     printf(" is %d\n", sum);
 }
-
