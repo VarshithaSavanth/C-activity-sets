b37ab4badac55c90a1f7ad98df0c7cbc95b6fba1
Varshitha Savanth <varshithas.bsc23@rvu.edu.in>
2024-02-17 21:34:59 +0530

    test

--- set02/problem05.c
+++ set02/problem05.c
@@ -8,5 +8,35 @@ void output(int a, int b, int gcd);
 
 int main()
 {
-  
+  int a = input();
+  int b = input();
+  int gcd = find_gcd(a, b);
+  output(a, b, gcd);
+  return 0;
+}
+
+int input()
+{
+  int x;
+  printf("Enter the value of a and b: ");
+  scanf("%d", &x);
+  return x;
+}
+
+int find_gcd(int a, int b)
+{
+  int i=0, gcd=0;
+  for (i=1 ; i<=a && i<=b ; i++)
+  {
+    if (a % i == 0 && b % i == 0)
+    {
+      gcd = i;
+    }
+  }
+  return gcd;
+}
+
+void output(int a, int b, int gcd)
+{
+  printf("GCD of %d and %d is :  %d\n",a, b, gcd);
 }
--- set02/problem06.c
+++ set02/problem06.c
@@ -8,5 +8,29 @@ void output(char *a, char *reverse_a);
 
 int main()
 {
-  
+  char a[100], str[100], rev_str[100], reverse_a[100];
+  input_string(a);
+  str_reverse(str, rev_str);
+  output(a, reverse_a);
+}
+
+void input_string(char *a)
+{
+  printf("Enter the string value : ");
+  scanf("%s", a);
+}
+void str_reverse(char *str, char *rev_str)
+{
+  char a = str;
+  str = rev_str;
+  rev_str = 
+  int i=0;
+  for (i=0 ; str[i]!='\0' ; i++)
+  {
+
+  }
+}
+void output(char *a, char *reverse_a)
+{
+
 }
