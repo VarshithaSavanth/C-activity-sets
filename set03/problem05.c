69981a5223077982d2a8661c2bfa67412069e654
Varshitha Savanth <varshithas.bsc23@rvu.edu.in>
2024-03-04 15:02:02 +0530

    test

--- set03/problem05.c
+++ set03/problem05.c
@@ -28,29 +28,39 @@ int input_array_size()
 void init_array(int n, int a[n])
 {
   int i = 0;
-  for (int i = 2 ; i <= n ; i++)
+  for (int i = 2; i <= n; i++)
   {
-    a[i] = i + 1;
+    a[i] = i;
   }
 }
 
 void erotosthenes_sieve(int n, int a[n])
 {
-  int i = 0;
-  for (i = 2 ; i <= n ; i++)
+  int i, j;
+  for (i = 2; i <= n; i++)
   {
-    if (n % i != 0)
+    for (j = i; j <= n; j = j + i)
     {
-      printf("%d", a[i]);
+      printf("%d\n", j);
+      a[j = j + i] = 0;
     }
   }
 }
 
 void output(int n, int a[n])
 {
-  printf("%d", a[0]);
-  for (int i = 1; i < n; i++)
+
+  int j;
+  printf("Prime numbers : ");
+  for (int i = 2; i < n; i++)
   {
-    printf(",%d ", a[i]);
+    if (a[i] != 0)
+    {
+      printf("%d", a[i]);
+      if (i < n - 1)
+      {
+        printf(", ");
+      }
+    }
   }
 }
